fda5951d7f3bb7cbcf8006e6f8a194f93203fbe1
GNVageesh <vageeshgn2005@gmail.com>
2021-08-12 11:38:53 +0530

    init

---
+++ pages/[id].js
@@ -0,0 +1,42 @@
+export const getStaticPaths = async () => {
+	const res = await fetch(
+		"https://express-student-api-production.up.railway.app/api/students"
+	);
+	const data = await res.json();
+
+	const paths = data.map((doc) => {
+		return {
+			params: { id: doc._id.toString() },
+		};
+	});
+
+	return {
+		paths,
+		fallback: false,
+	};
+};
+
+export const getStaticProps = async (context) => {
+	const id = context.params.id;
+	const res = await fetch(
+		"https://express-student-api-production.up.railway.app/api/students/" +
+			id
+	);
+	const data = res.json();
+
+	return {
+		props: { student: data },
+	};
+};
+
+const DataView = ({ student }) => {
+	return (
+		<div>
+			<h1>{student.name}</h1>
+			<h1>{student.roll}</h1>
+			<h1>{student.present}</h1>
+		</div>
+	);
+};
+
+export default DataView;
